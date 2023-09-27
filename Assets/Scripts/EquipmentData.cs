478e8dd5aa6f960bf52e3dbaf2b17d33658c32ef
svetoslavst <114879175+SvetoslavStoyanov66@users.noreply.github.com>
2023-09-27 23:02:11 +0300

    Data storage for Invetory

---
+++ Assets/Scripts/EquipmentData.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(menuName = "Items/EquipmentTool")]
+
+public class EquipmentData : ItemData
+{
+    public enum ToolType 
+    {
+        Hoe, WateringCan, Axe, Pickaxe
+    }
+    public ToolType toolType;
+
+}
---
+++ Assets/Scripts/ItemData.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(menuName = "Items/Item")]
+public class ItemData : ScriptableObject
+{
+    public string description;
+    public Sprite thumbnail;
+    public GameObject gameModel;
+}
