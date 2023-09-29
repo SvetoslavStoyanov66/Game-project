dfde040c0c6dd7010d7a479a5ae2b6a5d5cd5ffe
svetoslavst <114879175+SvetoslavStoyanov66@users.noreply.github.com>
2023-09-29 21:34:34 +0300

    upgraded timer dispaly

--- Assets/Scripts/UI/Timer.cs
+++ Assets/Scripts/UI/Timer.cs
@@ -4,16 +4,41 @@ using UnityEngine.UI;
 public class Timer : MonoBehaviour
 {
     public Text timerText;
+    public Text DaysText;
+    public Text SeasonText;
+    public Text DayInWeekText;
     public float secondsPerGameMinute = 1.0f;
-    public int fontSize = 24;  // Adjust this value to change the font size
+    public int fontSize = 24;
 
     private int hours = 6;
     private int minutes = 0;
     private float elapsedTime;
+    public int year;
+    public int day;
+    public int seasonNum;
+    public enum Season
+    {
+        Spring,
+        Summer,
+        Fall,
+        Winter
+    }
+    public enum Week
+    {
+        Saturday,
+        Sunday,
+        Monday,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday
+    }
+
+    public Season season;
+    public Week week;
 
     private void Start()
     {
-        // Set the font size
         timerText.fontSize = fontSize;
     }
 
@@ -36,14 +61,51 @@ public class Timer : MonoBehaviour
             {
                 minutes = 0;
                 hours++;
-                if (hours >= 24)
-                    hours = 0;
             }
         }
+        if (hours >= 24)
+        {
+            hours = 0;
+            day++;
+        }
+       
+            if (day >= 30)
+            {
+                day = 1;
+                seasonNum++;
+                if (seasonNum >= 4)
+                {
+                    seasonNum = 0;
+                }
+            }
+        
+
+        // Update the day of the week
+        week = GetDayOfTheWeek();
+    }
+
+    public Week GetDayOfTheWeek()
+    {
+        int dayPassed = YearsToDays(year) + SeasonToDays(seasonNum) + day;
+        int DayIndex = dayPassed % 7;
+        return (Week)DayIndex;
+    }
+
+    public static int YearsToDays(int years)
+    {
+        return years * 4 * 30;
+    }
+
+    public static int SeasonToDays(int season)
+    {
+        return season * 30;
     }
 
     private void UpdateUI()
     {
         timerText.text = $"{hours:00}:{minutes:00}";
+        DaysText.text = day.ToString();
+        SeasonText.text = season.ToString();
+        DayInWeekText.text = week.ToString();
     }
 }
