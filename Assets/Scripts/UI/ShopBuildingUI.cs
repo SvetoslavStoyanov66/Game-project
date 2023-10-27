09d5dcfecb891a736ac600488e869be8b7fcef4e
svetoslavst <114879175+SvetoslavStoyanov66@users.noreply.github.com>
2023-10-27 12:38:49 +0300

    Building Shop Basic UI

---
+++ Assets/Scripts/UI/ShopBuildingUI.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShopBuildingUI : MonoBehaviour
+{
+    enum BuildingPageUI
+    {
+        ChikenBuilding,CowBuilding
+    }
+    [SerializeField]
+    Image buildingImage;
+    [SerializeField]
+    Text nameText;
+    [SerializeField]
+    Text discriptionText;
+    [SerializeField]
+    Text moneyText;
+    [SerializeField]
+    Text woodText;
+    [SerializeField]
+    Text stoneText;
+    private BuildingPageUI currentPage = BuildingPageUI.ChikenBuilding;
+    private void ChangingUiPages(BuildingPageUI buildingPageUI)
+    {
+        switch (buildingPageUI)
+        {
+           case BuildingPageUI.ChikenBuilding:
+                buildingImage = null;
+                nameText.text = "Chiken coop";
+                discriptionText.text = "Building for chikens";
+                moneyText.text = "800";
+                woodText.text = "300";
+                stoneText.text = "100";
+                break;
+            case BuildingPageUI.CowBuilding:
+                buildingImage = null;
+                nameText.text = "Cowshed";
+                discriptionText.text = "Building for cows";
+                moneyText.text = "1200";
+                woodText.text = "400";
+                stoneText.text = "200";
+                break;
+        }
+    }
+    public void LeftButtonFunction()
+    {
+        int currentIndex = (int)currentPage;
+        currentIndex = (currentIndex + 1) % Enum.GetValues(typeof(BuildingPageUI)).Length;
+        currentPage = (BuildingPageUI)currentIndex;
+        ChangingUiPages(currentPage);
+    }
+    public void RightButtonFunction()
+    {
+        int currentIndex = (int)currentPage;
+        currentIndex = (currentIndex - 1 + Enum.GetValues(typeof(BuildingPageUI)).Length) % Enum.GetValues(typeof(BuildingPageUI)).Length;
+        currentPage = (BuildingPageUI)currentIndex;
+        ChangingUiPages(currentPage);
+    }
+}
