45bac34fb6e277adf779b6492023aeac9b3cf63c
svetoslavst <114879175+SvetoslavStoyanov66@users.noreply.github.com>
2024-01-10 18:28:41 +0200

    npc animations script

---
+++ Assets/Scripts/NPCanimations.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCanimations : MonoBehaviour
+{
+    Animator animator;
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+    
+    public void ActivateAnimations()
+    {
+        animator.SetBool("talking" ,true);
+    }
+    public void DeactivateAnimations()
+    {
+        animator.SetBool("talking" ,false);
+    }
+}
