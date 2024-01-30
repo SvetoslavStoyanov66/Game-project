6008705044203e0b001dc61b007627fd96121dfe
svetoslavst <114879175+SvetoslavStoyanov66@users.noreply.github.com>
2024-01-30 17:37:48 +0200

    Ui improvements

--- Assets/Scripts/UI/AnimalFoodShopSlot.cs
+++ Assets/Scripts/UI/AnimalFoodShopSlot.cs
@@ -10,6 +10,10 @@ public class AnimalFoodShopSlot : MonoBehaviour
     ItemData animalFoodData;
     [SerializeField]
     int priece;
+    [SerializeField]
+    Image clickImage;
+    [SerializeField]
+    Text notEnoughMoneyNot;
     private void Start() {
         Transform iconBorderTransform = this.gameObject.transform.GetChild(0);
         Image image = iconBorderTransform.gameObject.GetComponent<Image>();
@@ -18,7 +22,28 @@ public class AnimalFoodShopSlot : MonoBehaviour
 
     public void PurchaseItem()
     {
-        Inventory.Instance.HarvestCrops(animalFoodData);
-        Money.Instance.BuingItems(priece);
+        StartCoroutine(clickAnimation());
+        if (Money.Instance.moneyAmount > 0 && Money.Instance.moneyAmount - priece >= 0)
+        {
+            Money.Instance.BuingItems(priece);
+            notEnoughMoneyNot.text = "";
+            Inventory.Instance.HarvestCrops(animalFoodData);
+        }
+        else 
+        {
+            StartCoroutine(TextDuration(4));
+        }
+    }
+    IEnumerator clickAnimation()
+    {
+        clickImage.enabled = true;
+        yield return new WaitForSeconds(0.1f);
+        clickImage.enabled = false;
+    }
+    IEnumerator TextDuration(int num)
+    {
+        notEnoughMoneyNot.text = "Not enough money";
+        yield return new WaitForSeconds(num);
+        notEnoughMoneyNot.text = "";
     }
 }
