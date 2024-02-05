15f057fff7b45aa49240f9f46ab396327545a74a
svetoslavst <svetoslav.stoyanov66@gmail.com>
2024-02-05 20:59:13 +0200

    Space bug fix

---
+++ Assets/Scripts/spaceDis.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spaceDis : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("Space key press detected and ignored.");
+        }
+    }
+}
