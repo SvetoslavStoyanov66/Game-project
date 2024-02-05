3148e62a595a0016bfb7c1e88c40175e5c35eec5
svetoslavst <svetoslav.stoyanov66@gmail.com>
2024-02-05 18:49:23 +0200

    Item selling bug fixes

--- Assets/Scripts/UI/InventorySlot.cs
+++ Assets/Scripts/UI/InventorySlot.cs
@@ -71,34 +71,43 @@ public class InventorySlot : MonoBehaviour, IPointerEnterHandler, IPointerExitHa
         }
         else if (itemToDisplay != null && Shop.Instance.isShopOpen)
         {
-            if (Inventory.Instance.inventoryItems[slotIndex] is FoodData || Inventory.Instance.inventoryItems[slotIndex] is SeedsData)
+            if (Inventory.Instance != null && slotIndex >= 0 && slotIndex < Inventory.Instance.inventoryItems.Length && Inventory.Instance.inventoryItems[slotIndex] != null)
             {
-                (Inventory.Instance.inventoryItems[slotIndex]).quantity--;
-                if ((Inventory.Instance.inventoryItems[slotIndex]).quantity <= 0)
+                var item = Inventory.Instance.inventoryItems[slotIndex]; // Cache for readability and performance.
+                
+                // Decrement item quantity safely.
+                item.quantity--;
+
+                // Process selling based on item type.
+                if (item is FoodData foodItem)
+                {                  
+                    int foodPrice = foodItem.sellPrice;
+                    if (Money.Instance != null) Money.Instance.moneyAmount += foodPrice;
+                }
+                else if (item is SeedsData seedItem)
+                {
+                    double sellAmount = Convert.ToDouble(seedItem.price) * 0.75;
+                    sellAmount = Math.Round(sellAmount, 2);
+                    int sellAmountInt = Convert.ToInt32(sellAmount);
+                    if (Money.Instance != null) Money.Instance.moneyAmount += sellAmountInt;
+                }
+
+                // Remove item from inventory if quantity is less than 1.
+                if (item.quantity < 1)
                 {
                     for (int i = 0; i < Inventory.Instance.inventoryItems.Length; i++)
                     {
-                        if (Inventory.Instance.inventoryItems[i] != null && Inventory.Instance.inventoryItems[i].name == Inventory.Instance.inventoryItems[slotIndex].name)
+                        if (Inventory.Instance.inventoryItems[i] != null && Inventory.Instance.inventoryItems[i].name == item.name)
                         {
-                            Inventory.Instance.inventoryItems[i].quantity = 1;
+                            Inventory.Instance.inventoryItems[i].quantity = 0; // Set quantity to 0 or handle as needed.
                             Inventory.Instance.inventoryItems[i] = null;
                         }
                     }
                 }
-                if (Inventory.Instance.inventoryItems[slotIndex] is FoodData)
-                {                  
-                    foodPrice = (Inventory.Instance.inventoryItems[slotIndex] as FoodData).sellPrice;
-                    Money.Instance.moneyAmount += foodPrice;
-                }
-                else if (Inventory.Instance.inventoryItems[slotIndex] is SeedsData)
-                {
-                    double sellAmount = Convert.ToDouble((Inventory.Instance.inventoryItems[slotIndex] as SeedsData).price) * 0.75;
-                    sellAmount = Math.Round(sellAmount, 2);
-                    int sellAmountInt = Convert.ToInt32(sellAmount);
-                    Money.Instance.moneyAmount += sellAmountInt;
-                    seedPrice = sellAmountInt;
-                }
-            }       
+            }
+            UImanager.Instance.RenderHotbar();
+            UImanager.Instance.RenderInventory();
+
         }
     }
 
@@ -117,16 +126,16 @@ public class InventorySlot : MonoBehaviour, IPointerEnterHandler, IPointerExitHa
                 sellAmount = Math.Round(sellAmount, 2);
                 int sellAmountInt = Convert.ToInt32(sellAmount);
                 seedPrice = sellAmountInt;
-                //itemPriceText.text = "Sell price - " +seedPrice.ToString() + "G";
+                itemPriceText.text = "Sell price - " +seedPrice.ToString() + "G";
             }
             else if ((Inventory.Instance.inventoryItems[slotIndex]) is FoodData)
             {
                 foodPrice = (Inventory.Instance.inventoryItems[slotIndex] as FoodData).sellPrice;
-                //itemPriceText.text = "Sell price - " + foodPrice.ToString() + "G";
+                itemPriceText.text = "Sell price - " + foodPrice.ToString() + "G";
             }
             else
             {
-                //itemPriceText.text = "Unsellable";
+                itemPriceText.text = "Unsellable";
             }
         }
     }
@@ -142,15 +151,15 @@ public class InventorySlot : MonoBehaviour, IPointerEnterHandler, IPointerExitHa
         {
             if ((Inventory.Instance.inventoryItems[slotIndex]) is SeedsData)
             {
-                //itemPriceText.text = "";
+                itemPriceText.text = "";
             }
             else if ((Inventory.Instance.inventoryItems[slotIndex]) is FoodData)
             {
-                //itemPriceText.text = "";
+                itemPriceText.text = "";
             }
             else
             {
-                //itemPriceText.text = "";
+                itemPriceText.text = "";
             }
         }
     }
