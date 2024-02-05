afc13a6774afe15e1248ae32ac2740e83be499cf
svetoslavst <svetoslav.stoyanov66@gmail.com>
2024-02-05 23:44:35 +0200

    Menus addition

---
+++ Assets/Scripts/EscMenuManager.cs
@@ -0,0 +1,33 @@
+﻿
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EscMenuManager : MonoBehaviour
+{
+    [SerializeField]
+    GameObject ESCmenu;
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape) && !ESCmenu.activeSelf)
+        {
+            ESCmenu.SetActive(true);
+        }    
+        else if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            ESCmenu.SetActive(false);
+        }
+    }
+    public void CouninueButtonFunction()
+    {
+        ESCmenu.SetActive(false);
+    }
+    public void BackToMenuButtonFunction()
+    {
+        SceneManager.LoadScene(0);
+    }
+    public void ExitGameButtonFunction()
+    {
+        Application.Quit();
+    }
+}
---
+++ Assets/Scripts/MainMenuManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuManager : MonoBehaviour
+{
+    public void PlayButtonFunction()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void LeaveButtonFunction()
+    {
+        Application.Quit();
+    }
+}
