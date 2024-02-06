0734a68d0fd5876a9eaba0749cfccc3696dbb0fa
svetoslavst <svetoslav.stoyanov66@gmail.com>
2024-02-06 21:58:05 +0200

    coop ui improvements

---
+++ Assets/Scripts/Egg.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Egg : MonoBehaviour
+{
+    [SerializeField]
+    Coop coop;
+    private void Start()
+    {
+        coop = FindObjectOfType<Coop>();
+    }
+     void OnDestroy()
+    {
+        coop.EggTextValueAssigning(-1);
+    }
+}
