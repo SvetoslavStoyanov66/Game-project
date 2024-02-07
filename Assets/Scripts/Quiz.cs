0148f6dc12933ee505962f5c346fb01050a266d4
svetoslavst <svetoslav.stoyanov66@gmail.com>
2024-02-07 17:45:03 +0200

    basic quiz logic

--- Assets/Scripts/Quiz.cs
+++ Assets/Scripts/Quiz.cs
@@ -1,18 +1,69 @@
-﻿using System.Collections;
+﻿
 using System.Collections.Generic;
+using System.Data;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Quiz : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField]
+    Button button;
+    [SerializeField]
+    Button button1;
+    [SerializeField]
+    Button button2;
+    int reward;
+    List<FoodData.QuestionAndAnswers> questionAndAnswers = new List<FoodData.QuestionAndAnswers>();
+    [SerializeField]
+    GameObject notifier;
+    bool canInteract = false;
 
-    // Update is called once per frame
-    void Update()
+    public void AddQustionDataToList(FoodData.QuestionAndAnswers data)
     {
-        
+        questionAndAnswers.Add(data);
+    }
+    private void ButtonAndRewardAssignment()
+    {
+        if (questionAndAnswers.Count > 0)
+        {
+            int index = Random.Range(0, questionAndAnswers.Count);
+            FoodData.QuestionAndAnswers selectedQA = questionAndAnswers[index];
+
+            button.GetComponentInChildren<Text>().text = selectedQA.question;
+            button1.GetComponentInChildren<Text>().text = selectedQA.question;
+            button2.GetComponentInChildren<Text>().text = selectedQA.question;
+
+            List<string> answers = new List<string> { selectedQA.correctAnswer, selectedQA.wrongAnswer, selectedQA.wrongAnswer2 };
+            Shuffle(answers);
+
+            button.GetComponentInChildren<Text>().text = answers[0];
+            button1.GetComponentInChildren<Text>().text = answers[1];
+            button2.GetComponentInChildren<Text>().text = answers[2];
+
+            reward = questionAndAnswers.Count * 20;
+        }
+    }
+    private void Shuffle<T>(List<T> list)
+    {
+        int n = list.Count;
+        while (n > 1)
+        {
+            n--;
+            int k = Random.Range(0, n + 1);
+            T value = list[k];
+            list[k] = list[n];
+            list[n] = value;
+        }
+    }
+    private void OnTriggerEnter()
+    {
+        notifier.SetActive(true);
+        canInteract = true;
+    }
+    private void OnTriggerExit()
+    {
+        notifier.SetActive(false);
+        canInteract = false;
+
     }
 }
