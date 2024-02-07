a8a9d4e1946efa1bf6a0a9320a05f730abedc086
svetoslavst <svetoslav.stoyanov66@gmail.com>
2024-02-07 15:55:54 +0200

    small changes

--- Assets/Scripts/Inventory/FoodData.cs
+++ Assets/Scripts/Inventory/FoodData.cs
@@ -4,6 +4,18 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "Items/Food")]
 public class FoodData : ItemData
 {
+      [System.Serializable]
+    public struct QuestionAndAnswers
+    {
+        public string question;
+        public string correctAnswer;
+        public string wrongAnswer;
+        
+        public string wrongAnswer2;
+    }
     public int energyFillAmount;
     public int sellPrice;
+    public QuestionAndAnswers quiz1;
+    public QuestionAndAnswers quiz2;
+    public QuestionAndAnswers quiz3;
 }
